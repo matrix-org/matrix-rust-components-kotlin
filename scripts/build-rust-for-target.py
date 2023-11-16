a3bd38c9b698b58b1a34c9979ead6b858b6f8153
Jorge Martin Espinosa <jorgem@element.io>
2023-11-16 16:37:30 +0100

    Create parallelised SDK release workflow (#18)
    This is in a separate workflow so we can experiment with it until it's stable

---
+++ scripts/build-rust-for-target.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import re
+import subprocess
+from enum import Enum, auto
+from tempfile import TemporaryDirectory
+
+class Module(Enum):
+    SDK = auto()
+    CRYPTO = auto()
+
+def module_type(value):
+    try:
+        return Module[value.upper()]
+    except KeyError:
+        raise argparse.ArgumentTypeError(
+            f"Invalid module choice: {value}. Available options: SDK, CRYPTO")
+
+
+def get_build_version_file_path(module: Module, project_root: str) -> str:
+    if module == Module.SDK:
+        return os.path.join(project_root, 'buildSrc/src/main/kotlin', 'BuildVersionsSDK.kt')
+    elif module == Module.CRYPTO:
+        return os.path.join(project_root, 'buildSrc/src/main/kotlin', 'BuildVersionsCrypto.kt')
+    else:
+        raise ValueError(f"Unknown module: {module}")
+
+
+def read_version_numbers_from_kotlin_file(file_path):
+    with open(file_path, "r") as file:
+        content = file.read()
+
+    major_version = int(re.search(r"majorVersion\s*=\s*(\d+)", content).group(1))
+    minor_version = int(re.search(r"minorVersion\s*=\s*(\d+)", content).group(1))
+    patch_version = int(re.search(r"patchVersion\s*=\s*(\d+)", content).group(1))
+
+    return major_version, minor_version, patch_version
+
+
+def clone_repo_and_checkout_ref(directory, git_url, ref):
+    # Clone the repo
+    subprocess.run(
+        ["git", "clone", git_url, directory],
+        check=True,
+        text=True,
+    )
+    # Checkout the specified ref (commit hash, tag, or branch)
+    subprocess.run(
+        ["git", "checkout", ref],
+        cwd=directory,
+        check=True,
+        text=True,
+    )
+
+def get_linkable_ref(directory, ref):
+    result = subprocess.run(
+        ["git", "show-ref", "--verify", f"refs/heads/{ref}"],
+        cwd=directory,
+        capture_output=True,
+        text=True
+    )
+    if result.returncode == 0:
+        # The reference is a valid branch name, return the corresponding commit hash
+        return result.stdout.strip().split()[0]
+
+    # The reference is not a valid branch name, return the reference itself
+    return ref
+
+
+def is_provided_version_higher(major: int, minor: int, patch: int, provided_version: str) -> bool:
+    provided_major, provided_minor, provided_patch = map(int, provided_version.split('.'))
+    if provided_major > major:
+        return True
+    elif provided_major == major:
+        if provided_minor > minor:
+            return True
+        elif provided_minor == minor:
+            return provided_patch > patch
+    return False
+
+
+def execute_build_script(script_directory: str, sdk_path: str, module: Module, target: str):
+    print("Execute build script...")
+    build_script_path = os.path.join(script_directory, "build-rust-for-target.sh")
+    subprocess.run(
+        ["/bin/bash", build_script_path, "-p", sdk_path, "-m", module.name.lower(), "-t", target, "-r"],
+        check=True,
+        text=True
+    )
+    print("Finish executing build script with success")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--module", type=module_type, required=True,
+                    help="Choose a module (SDK or CRYPTO)")
+parser.add_argument("-v", "--version", type=str, required=True,
+                    help="Version as a string (e.g. '1.0.0')")
+parser.add_argument("-t", "--target", type=str, required=True,
+                    help="Choose a target ('arm64-v8a', 'armv7a', 'x86', 'x86_64')")
+parser.add_argument("-r", "--ref", type=str, required=True,
+                    help="Ref to the git matrix-rust-sdk (branch name, commit or tag)")
+parser.add_argument("-p", "--path-to-sdk", type=str, required=False,
+                    help="Choose a module (SDK or CRYPTO)")
+parser.add_argument("-s", "--skip-clone", action="store_true", required=False,
+                    help="Skip cloning the Rust SDK repository")
+
+args = parser.parse_args()
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir).rstrip(os.sep)
+sdk_git_url = "https://github.com/matrix-org/matrix-rust-sdk.git"
+
+if args.path_to_sdk:
+    sdk_path = args.path_to_sdk
+else:
+    sdk_path = TemporaryDirectory().name
+
+skip_clone = args.skip_clone
+
+print(f"Project Root Directory: {project_root}")
+print(f"Selected module: {args.module}")
+print(f"Version: {args.version}")
+print(f"SDK git ref: {args.ref}")
+
+build_version_file_path = get_build_version_file_path(args.module, project_root)
+major, minor, patch = read_version_numbers_from_kotlin_file(build_version_file_path)
+if is_provided_version_higher(major, minor, patch, args.version):
+    print(
+        f"The provided version ({args.version}) is higher than the previous version ({major}.{minor}.{patch}) so we can start the release process")
+else:
+    print(
+        f"The provided version ({args.version}) is not higher than the previous version ({major}.{minor}.{patch}) so bump the version before retrying.")
+    exit(0)
+
+if skip_clone is False:
+    clone_repo_and_checkout_ref(sdk_path, sdk_git_url, args.ref)
+
+linkable_ref = get_linkable_ref(sdk_path, args.ref)
+execute_build_script(current_dir, sdk_path, args.module, args.target)
---
+++ scripts/build-rust-for-target.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+set -e
+
+helpFunction() {
+  echo ""
+  echo "Usage: $0 -r -p /Documents/dev/matrix-rust-sdk -m sdk"
+  echo -e "\t-p Local path to the rust-sdk repository"
+  echo -e "\t-o Optional output path with the expected name of the aar file"
+  echo -e "\t-r Flag to build in release mode"
+  echo -e "\t-m Option to select the gradle module to build. Default is sdk"
+  echo -e "\t-t Option to to select an android target to build against."
+  exit 1
+}
+
+scripts_dir=$(
+  cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+  pwd -P
+)
+
+is_release='false'
+gradle_module='sdk'
+only_target=''
+output=''
+
+while getopts ':rp:m:t:o:' 'opt'; do
+  case ${opt} in
+  'r') is_release='true' ;;
+  'p') sdk_path="$OPTARG" ;;
+  'm') gradle_module="$OPTARG" ;;
+  't') only_target='arm64-v8a' ;;
+  'o') output="$OPTARG" ;;
+  ?) helpFunction ;;
+  esac
+done
+
+if [ -z "$sdk_path" ]; then
+  echo "sdk_path is empty, please provide one"
+  helpFunction
+fi
+
+if [ -z "$only_target" ]; then
+  helpFunction
+else
+  target_command=("--only-target" "$only_target")
+fi
+
+if [ "$gradle_module" = "crypto" ]; then
+  ## NDK is needed for https://crates.io/crates/olm-sys
+  if [ -z "$ANDROID_NDK" ]; then
+    echo "please set the ANDROID_NDK environment variable to your Android NDK installation"
+    exit 1
+  fi
+  src_dir="$scripts_dir/../crypto/crypto-android/src/main"
+  package="crypto-sdk"
+else
+  src_dir="$scripts_dir/../sdk/sdk-android/src/main"
+  package="full-sdk"
+fi
+
+if ${is_release}; then
+  profile="release"
+else
+  profile="reldbg"
+fi
+
+echo "Launching build script with following params:"
+echo "sdk_path = $sdk_path"
+echo "profile = $profile"
+echo "gradle_module = $gradle_module"
+echo "src-dir = $src_dir"
+
+pushd "$sdk_path" || exit 1
+
+cargo xtask kotlin build-android-library --profile "$profile" "${target_command[@]}" --src-dir "$src_dir" --package "$package"
+
+pushd "$scripts_dir/.." || exit 1
+
+shift $((OPTIND - 1))
+
+moveFunction() {
+  if [ -z "$output" ]; then
+    echo "No output path provided, keep the generated path"
+  else
+    mv "$1" "$output"
+  fi
+}
+
+## For now, cargo ndk includes all generated so files from the target directory, so makes sure it just includes the one we need.
+echo "Clean .so files"
+if [ "$gradle_module" = "crypto" ]; then
+  find crypto/crypto-android/src/main/jniLibs -type f ! -name 'libmatrix_sdk_crypto_ffi.so' -delete
+else
+  find sdk/sdk-android/src/main/jniLibs -type f ! -name 'libmatrix_sdk_ffi.so' -delete
+fi
