76e7a1c53fd4645db41d0a045b3f83adc31b5ca7
Jorge Martín <jorgem@element.io>
2024-03-01 14:20:54 +0100

    Add `androidx.annotation` library so we can use `@RequiresApi`
    This is needed in new versions of UniFFi.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,6 +5,7 @@ object DependenciesVersions {
     const val nexusPublishGradlePlugin = "1.3.0"
     const val jna = "5.14.0"
     const val coroutines = "1.7.3"
+    const val annotations = "1.7.1"
 }
 
 /**
@@ -14,4 +15,5 @@ object Dependencies {
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DependenciesVersions.coroutines}"
     const val junit = "junit:junit:${DependenciesVersions.jUnit}"
     const val jna = "net.java.dev.jna:jna:${DependenciesVersions.jna}@aar"
+    const val annotations = "androidx.annotation:annotation:${DependenciesVersions.annotations}"
 }
