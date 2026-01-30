b991bb44ee4d5355f4097d4d54c43eb9c2a646e3
Jorge Martín <jorgem@element.io>
2026-01-30 14:48:38 +0100

    Add support for publishing snapshots

--- buildSrc/src/main/kotlin/BuildVersionsSDK.kt
+++ buildSrc/src/main/kotlin/BuildVersionsSDK.kt
@@ -1,5 +1,5 @@
 object BuildVersionsSDK {
-	const val majorVersion = 26
-	const val minorVersion = 1
-	const val patchVersion = 27
+	const val majorVersion = "26"
+	const val minorVersion = "1"
+	const val patchVersion = "27"
 }
